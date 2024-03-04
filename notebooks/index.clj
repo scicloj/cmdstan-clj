1f69567e19febaf9a5f00f8aeaa38018565a6c2c
daslu <daniel.slutsky@gmail.com>
2024-03-04 23:43:51 +0200

    initial working example

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,1 +1,44 @@
-(ns index)
+;; # cmdstan-clj
+
+(ns index
+  (:require [tablecloth.api :as tc]
+            [clojure.java.shell :as shell]
+            [charred.api :as charred]
+            [clojure.java.io :as io]
+            [scicloj.kindly.v4.kind :as kind]))
+
+
+;; ## Walkthrough
+
+;; (reproducing [CmdStanR's intro](https://mc-stan.org/cmdstanr/articles/cmdstanr.html))
+
+(def cmdstan-path (System/getenv "CMDSTAN_PATH"))
+
+(def model-path
+  (str (System/getProperty "user.dir")
+       "/notebooks/bernoulli"))
+
+(shell/sh "make" model-path
+          :dir cmdstan-path)
+
+(def data
+  {:N 10
+   :y [0 1 0 0 0 0 0 0 0 1]})
+
+
+(def json-path "temp/bernoulli.data.json")
+
+(io/make-parents json-path)
+
+(charred/write-json json-path data)
+
+(def samples-path "temp/bernoulli.samples.csv")
+
+(-> (shell/sh model-path "sample"
+              "data" (str "file=" json-path)
+              "output" (str "file=" samples-path))
+    :out
+    kind/code)
+
+(-> samples-path
+    tc/dataset)
