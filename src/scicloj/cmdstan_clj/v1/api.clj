23ee2abb64845c599b61f897462d5cff17d18cc3
daslu <daniel.slutsky@gmail.com>
2024-03-09 00:42:13 +0200

    made the API draft work (various fixes)

--- src/scicloj/cmdstan_clj/v1/api.clj
+++ src/scicloj/cmdstan_clj/v1/api.clj
@@ -3,26 +3,31 @@
             [clojure.java.io :as io]
             [charred.api :as charred]
             [clojure.java.shell :as shell]
-            [tech.v3.dataset :as tmd]))
+            [tech.v3.dataset :as tmd]
+            [clojure.string :as str]))
 
 (def stan-home (System/getenv "STAN_HOME"))
 
-(defn stan-model [code]
-  (let [path (tempfiles/tempfile! "")]
-    (shell/sh "make" path
-              :dir stan-home)
-    {:path path}))
+(defn model [code]
+  (let [path-stan (:path (tempfiles/tempfile! ".stan"))
+        path (str/replace path-stan #"\.stan$" "")
+        _ (spit path-stan code)
+        ret (shell/sh "make" path
+                      :dir stan-home)]
+    (assoc ret
+           :path path)))
 
 (defn sample [model data]
-  (let [data-path (tempfiles/tempfile! "json")
-        samples-path (tempfiles/tempfile! "csv")
+  (let [data-path (:path (tempfiles/tempfile! ".json"))
+        samples-path (:path (tempfiles/tempfile! ".csv"))
         _ (charred/write-json data-path data)
         ret (shell/sh (:path model) "sample"
                       "data" (str "file=" data-path)
-                      "output" (str "file=" samples-path))]
+                      "output" (str "file=" samples-path))
+        samples (-> samples-path
+                    (tmd/->dataset {:key-fn keyword}))]
     (assoc ret
-           :samples (-> samples-path
-                        (tmd/->dataset {:key-fn keyword})
+           :samples (-> samples
                         (tmd/set-dataset-name "model samples")
-                        (tmd/add-column :i (fn [ds]
-                                             (-> ds tmd/row-count range)))))))
+                        (tmd/add-or-update-column :i
+                                                  (-> samples tmd/row-count range))))))
