c5a0a29c2f4bf299469f1df15b22cc9bc5e14dc1
daslu <daniel.slutsky@gmail.com>
2024-07-29 23:59:28 +0300

    version 1-alpha2

--- build.clj
+++ build.clj
@@ -15,7 +15,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'org.scicloj/cmdstan-clj)
-(def version "1-alpha1")
+(def version "1-alpha2")
 (def snapshot (str version "-SNAPSHOT"))
 (def class-dir "target/classes")
 
